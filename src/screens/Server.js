565ff199aa86ec4d6afbdeb1596193d80f370e56
milosoria <camilo.soria@uc.cl>
2022-09-25 19:43:59 -0300

    feat: version 1.0.3

--- src/screens/Server.js
+++ src/screens/Server.js
@@ -1,45 +1,45 @@
-import { useState } from 'react'
-import { Button, FlatList, Text, View } from 'react-native'
-import netHandler from '../utils/connection'
-import * as Network from 'expo-network'
+// import { useState } from 'react'
+// import { Button, FlatList, Text, View } from 'react-native'
+// import netHandler from '../utils/connection'
+// import * as Network from 'expo-network'
 
-const ServerScreen = ({ navigation }) => {
-    const [server, setServer] = useState(null)
-    const [chats, setChats] = useState([])
-    const [ip, setIp] = useState('')
-    const handleStartServer = async () => {
-        if (!server) setServer(netHandler.createServer(chats, setChats))
-        try {
-            let tempIp = await Network.getIpAddressAsync()
-            setIp(tempIp)
-        } catch (e) {
-            console.log(e.message)
-        }
-    }
-    const handleStopServer = () => {
-        if (server) {
-            server.close()
-            setServer(null)
-        }
-    }
+// const ServerScreen = ({ navigation }) => {
+//     const [server, setServer] = useState(null)
+//     const [chats, setChats] = useState([])
+//     const [ip, setIp] = useState('')
+//     const handleStartServer = async () => {
+//         if (!server) setServer(netHandler.createServer(chats, setChats))
+//         try {
+//             let tempIp = await Network.getIpAddressAsync()
+//             setIp(tempIp)
+//         } catch (e) {
+//             console.log(e.message)
+//         }
+//     }
+//     const handleStopServer = () => {
+//         if (server) {
+//             server.close()
+//             setServer(null)
+//         }
+//     }
 
-    return (
-        <View>
-            {ip.length > 0 ? <Text>Server Screen: {ip}</Text> : <Text>Server Screen</Text>}
-            <Button title="Start Server" onPress={handleStartServer} />
-            <Button title="Stop Server" onPress={handleStopServer} />
-            <Button title="Go to Client Screen" onPress={() => navigation.navigate('Client')} />
-            {server ? <Text>Server is on</Text> : null}
-            <FlatList
-                data={chats}
-                renderItem={({ item }) => {
-                    return <Text style={{ margin: 10, fontSize: 20 }}>{item.msg}</Text>
-                }}
-                keyExtractor={item => item.id}
-            />
-        </View>
-    )
-}
+//     return (
+//         <View>
+//             {ip.length > 0 ? <Text>Server Screen: {ip}</Text> : <Text>Server Screen</Text>}
+//             <Button title="Start Server" onPress={handleStartServer} />
+//             <Button title="Stop Server" onPress={handleStopServer} />
+//             <Button title="Go to Client Screen" onPress={() => navigation.navigate('Client')} />
+//             {server ? <Text>Server is on</Text> : null}
+//             <FlatList
+//                 data={chats}
+//                 renderItem={({ item }) => {
+//                     return <Text style={{ margin: 10, fontSize: 20 }}>{item.msg}</Text>
+//                 }}
+//                 keyExtractor={item => item.id}
+//             />
+//         </View>
+//     )
+// }
 
 
-export default ServerScreen
+// export default ServerScreen

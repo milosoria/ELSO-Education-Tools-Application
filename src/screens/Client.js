1459f7d27ef46534c0e3132ca51e624893913100
milosoria <camilo.soria@uc.cl>
2022-09-27 21:48:18 -0300

    feat: expo prebuild ran in order to use native code

--- src/screens/Client.js
+++ src/screens/Client.js
@@ -1,46 +1,51 @@
-// import { useEffect, useState } from 'react'
-// import { Button, FlatList, Text, TextInput, View } from 'react-native'
-// import * as Network from 'expo-network'
-// import netHandler from '../utils/connection'
+import { useEffect, useState } from 'react'
+import { Button, FlatList, Text, TextInput, View } from 'react-native'
+import * as Network from 'expo-network'
+import netHandler from '../utils/connection'
+import '../../shim'
 
 
 
-// const Client = () => {
+const Client = () => {
 
-//     const [client, setClient] = useState(null)
-//     const [chats, _] = useState([])
+    const [client, setClient] = useState(null)
+    const [chats, _] = useState([])
 
-//     useEffect(async () => {
-//         let ip = await Network.getIpAddressAsync()
-//         setClient(netHandler.createClient(ip))
+    useEffect(() => {
+        const configClient = async () => {
+            let ip = await Network.getIpAddressAsync()
+            setClient(netHandler.createClient(ip))
+        }
+        configClient()
+    }, [])
 
-//         return () => { }
-//     }, [])
+    return (
+        <View>
+            <Text>Client Screen</Text>
+            <Button title="Stop Client" onPress={() => {
+                if (client) {
+                    client.destroy()
+                    setClient(null)
+                }
+            }} />
+            {client ? <Text>Client is on</Text> : null}
+            <FlatList
+                data={chats}
+                renderItem={({ item }) => {
+                    return <Text style={{ margin: 10, fontSize: 20 }}>{item.msg}</Text>
+                }}
+                keyExtractor={item => item.id}
+            />
+            <TextInput placeholder="Enter a number" placeholderTextColor="black" style={{ margin: 10, borderWidth: 2, color: 'black' }} onSubmitEditing={
+                ({ nativeEvent: { text } }) => {
+                    if (client) {
+                        client.write(JSON.stringify({ msg: text, id: 1 }))
+                    }
+                }} />
+        </View>
+    )
+}
 
-//     return <View>
-//         <Text>Client Screen</Text>
-//         <Button title="Stop Client" onPress={() => {
-//             if (client) {
-//                 client.destroy()
-//                 setClient(null)
-//             }
-//         }} />
-//         {client ? <Text>Client is on</Text> : null}
-//         <FlatList
-//             data={chats}
-//             renderItem={({ item }) => {
-//                 return <Text style={{ margin: 10, fontSize: 20 }}>{item.msg}</Text>
-//             }}
-//             keyExtractor={item => item.id}
-//         />
-//         <TextInput placeholder="Type a message" placeholderTextColor="black" style={{ margin: 10, borderWidth: 2, color: 'black' }} onSubmitEditing={({ nativeEvent: { text } }) => {
-//             if (client) {
-//                 client.write(JSON.stringify({ msg: text, id: 1 }))
-//             }
-//         }} />
-//     </View>
-// }
 
 
-
-// export default Client
+export default Client
